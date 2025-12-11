57288785432eea3e2f629ef3adb7c267086e3e4f
muhammadjonsaidov <javascript0222@gmail.com>
2025-12-11 12:05:26 +0500

    completed

--- src/main/java/com/salesdoctor/audiotranscriberai/controller/TranscriptionController.java
+++ src/main/java/com/salesdoctor/audiotranscriberai/controller/TranscriptionController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/transcribe")
+@RequestMapping("/api/voice/upload")
 @CrossOrigin(originPatterns = "*")
 public class TranscriptionController {
 
